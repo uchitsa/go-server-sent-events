32770220a76f2daccc210c7ff6d8911d4e925e4d
kodsurfer <kodsurfer@ya.ru>
2021-09-25 14:01:31 +0300

    update main

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package go_server_sent_events
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"github.com/gofiber/adaptor/v2"
 	"github.com/gofiber/fiber/v2"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 type Dashboard struct {
@@ -18,6 +23,7 @@ type Client struct {
 func main() {
 	app := fiber.New()
 	app.Get("/sse", adaptor.HTTPHandler(handler(dashboardHandler)))
+	app.Listen(":3000")
 }
 
 func handler(f http.HandlerFunc) http.Handler {
@@ -31,8 +37,35 @@ func dashboardHandler(w http.ResponseWriter, r *http.Request) {
 		events: make(chan *Dashboard, 10),
 	}
 	go updateDashboard(client)
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Content-Type\"", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	timeout := time.After(1 * time.Second)
+	select {
+	case events := <-client.events:
+		var buf bytes.Buffer
+		encoder := json.NewEncoder(&buf)
+		encoder.Encode(events)
+		fmt.Fprintf(w, "data: %v\n\n", buf.String())
+		fmt.Printf("data: %v\n", buf.String())
+	case <-timeout:
+		fmt.Fprintf(w, ": nothing to sent\n\n")
+	}
+
+	if f, ok := w.(http.Flusher); ok {
+		f.Flush()
+	}
 }
 
 func updateDashboard(client *Client) {
-
+	for {
+		db := &Dashboard{
+			User: uint(rand.Uint32()),
+		}
+		client.events <- db
+	}
 }
